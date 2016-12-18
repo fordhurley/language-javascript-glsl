9f03a9ac65a7fdb98459b6b272f377d02ebea9d8
Ford Hurley <ford.hurley@gmail.com>
2016-12-18 12:33:41 -0500

    Forked to "Javascript (GLSL)" to highlight glsl in template strings

---
+++ sample.js
@@ -0,0 +1,29 @@
+var vertexShader = ` // glsl
+  attribute vec3 position;
+  attribute float time;
+
+  uniform float clock;
+
+  varying float age;
+
+  void main() {
+    age = clock - time;
+    gl_Position = vec4(position, 1.0);
+  }
+`
+
+var fragmentShader = `//glsl
+  varying float age;
+  void main() {
+    if (age < 0) { discard; }
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+  }
+`
+
+function foo() {
+  var x = 0;
+  for (var i = 0; i < 10; i++) {
+    x += i;
+  }
+  console.log(i);
+}
